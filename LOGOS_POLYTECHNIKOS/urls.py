d33ef009c139a1d743207116d691c26eb6558f6a
LightSpectr <maked440@gmail.com>
2021-11-27 19:16:06 +0100

    app init; bat files "pause" added; frontend dir added;

--- LOGOS_POLYTECHNIKOS/urls.py
+++ LOGOS_POLYTECHNIKOS/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from logos_app.views import (
+    index_view
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_view),
 ]
---
+++ logos_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LogosAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'logos_app'
