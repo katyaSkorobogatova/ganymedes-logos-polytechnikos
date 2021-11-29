376774adad64589c71d92901d9175cc9d4ee503a
LightSpectr <maked440@gmail.com>
2021-11-30 00:02:53 +0100

    models registration to admin tab

--- logos_app/admin.py
+++ logos_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+
+from .models import (Article, Magazine, Review)
+
+
 # Register your models here.
+admin.site.register(Article)
+admin.site.register(Magazine)
+admin.site.register(Review)
