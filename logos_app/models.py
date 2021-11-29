77cbf33c98fa3850a3c925659323f138b8b71ef8
LightSpectr <maked440@gmail.com>
2021-11-30 00:00:46 +0100

    MySQL -> Django model conversion (inspectdb)

--- logos_app/models.py
+++ logos_app/models.py
@@ -1,3 +1,56 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    pk_article = models.AutoField(primary_key=True)
+    id_autor = models.IntegerField()
+    name = models.CharField(max_length=255)
+    magazine_number = models.IntegerField()
+    text = models.TextField()
+    date_of_create = models.DateField()
+    edited = models.IntegerField(blank=True, null=True)
+    status = models.CharField(max_length=9)
+
+    class Meta:
+
+        db_table = 'article'
+
+
+
+class Magazine(models.Model):
+    pk_magazine = models.AutoField(primary_key=True)
+    magazine_number = models.IntegerField()
+    release_date = models.DateField()
+    published = models.IntegerField()
+    max_number_of_magazine = models.IntegerField()
+
+    class Meta:
+
+        db_table = 'magazine'
+
+
+class Review(models.Model):
+    pk_review = models.AutoField(primary_key=True)
+    pk_article = models.IntegerField()
+    id_reviewer = models.IntegerField()
+    id_editor = models.IntegerField()
+    relevancy = models.IntegerField()
+    interesting = models.IntegerField()
+    usefulness = models.IntegerField()
+    originality = models.IntegerField()
+    proffesional_level = models.IntegerField()
+    language_level = models.IntegerField()
+    stylistic_level = models.IntegerField()
+    commentary = models.TextField()
+    status = models.CharField(max_length=11)
+
+    class Meta:
+
+        db_table = 'review'
