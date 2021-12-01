4394f051ed5e9a3fa0c38adc5b1aa2e9d37fd75c
KonstantinZhidkovVSPJ <94126060+KonstantinZhidkovVSPJ@users.noreply.github.com>
2021-12-01 03:33:08 +0100

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -9,18 +9,39 @@ function append(parent, el) {
     return parent.appendChild(el);
 }
 
-const getArticles = (parent, child, className) => {
-    fetch(url)
-        .then(data => data.json())
-        .then(data => {
-            let news = data;
-            for (k in news) {
-                let childNode = createNode(child);
-                childNode.classList.add(className);
-                childNode.textContent = news[k];
-                append(parent, childNode);
-            }
-        })
+async function getArticles(url) {
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-getArticles(newsContainer, 'div', 'main__article');
+async function renderArticles(url, parent) {
+    let articles = await getArticles(url);
+    let html = '';
+    articles.forEach(article => {
+        let htmlSegment = `<div class="main__article">
+                                <div class="main__articleUpperPart">
+                                    <span class="main__articleTitle">${article.title}</span>
+                                    <br />
+                                    <span class="main__articleAuthor">${article.author}</span>
+                                </div>
+                                <div class="main__articleLowerPart">
+                                    <div class="main__articleText">${article.text}</div>
+                                    <span class="main__articleReadmore">
+                                        <a href=${article.url} target="__blank" class="main__articleSource">
+                                            <b>Číst více</b>
+                                        </a>
+                                    </span>
+                                </div>
+                            </div>`;
+
+        html += htmlSegment;
+    });
+
+    parent.innerHTML = html;
+}
+
+renderArticles(url, newsContainer);
