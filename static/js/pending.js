2ca65048bbf566ce10121771061b4ca164f5f4ff
KonstantinZhidkovVSPJ <94126060+KonstantinZhidkovVSPJ@users.noreply.github.com>
2022-01-17 01:07:13 +0100

    Reviewers dropdown list

--- static/js/pending.js
+++ static/js/pending.js
@@ -1,5 +1,6 @@
 const newsContainer = document.querySelector('.main__newsContainer');
 const url = 'load';
+const reviewersUrl = 'reviewerload';
 
 async function getArticles(url) {
     try {
@@ -10,8 +11,18 @@ async function getArticles(url) {
     }
 }
 
+function generateReviewers(reviewersArr) {
+    let html = '';
+
+    reviewersArr.forEach(reviewer => html += `<option value="${reviewer.name}">${reviewer.name}</option>`)
+
+    return html;
+}
+
 async function renderArticles(url, parent) {
     let articles = await getArticles(url);
+    let reviewers = await getArticles(reviewersUrl);
+    console.log(reviewers);
     let html = '';
     articles.forEach(article => {
         let htmlSegment = `<div class="main__article">
@@ -28,17 +39,29 @@ async function renderArticles(url, parent) {
                                     <div class="main__articleDateAndButtonsContainer">
                                         <span class="main__articleDate">${article.date_of_create}</span>
                                         <div class="main__articleButtonsContainer">
-                                            <div class="main__articleButton">
-                                                <a href="/article/${article.id}" class="main__articleButtonLink">Číst více</a>
+                                            <div class="main__articleButtonsContainer-left">
+                                                <div class="main__articleButton">
+                                                    <a href="/article/${article.id}" class="main__articleButtonLink">Číst více</a>
+                                                </div>
+                                                <div class="main__articleButton">
+                                                    <a href="/article/${article.id}/edit" class="main__articleButtonLink">Upravit</a>
+                                                </div>
                                             </div>
-                                            <div class="main__articleButton">
-                                                <a href="/article/${article.id}/edit" class="main__articleButtonLink">Upravit</a>
+                                            <div class="main__articleButtonsContainer-right">
+                                                <div class="main__articleButton">
+                                                    <a href="/article/${article.id}/delete" class="main__articleButtonLink">Odstranit</a>
+                                                </div>
+                                                <div class="main__articleButton">
+                                                    <a href="/pending/${article.id}/todraft" class="main__articleButtonLink">Poslat zpět autorovi</a>
+                                                </div>
                                             </div>
-                                            <div class="main__articleButton">
-                                                <a href="/article/${article.id}/delete" class="main__articleButtonLink">Odstranit</a>
-                                            </div>
-                                            <div class="main__articleButton">
-                                                <a href="/pending/${article.id}/todraft" class="main__articleButtonLink">Poslat zpět autorovi</a>
+                                            <div class="main__articleButtonsContainer-dropdown">
+                                                <form action="">
+                                                    <label for="reviewer">Zvolte si reviewera:</label>
+                                                    <select id="reviewer" name="reviewer">
+                                                        ${generateReviewers(reviewers)}
+                                                    </select>
+                                                </form>
                                             </div>
                                         </div>
                                     </div>
